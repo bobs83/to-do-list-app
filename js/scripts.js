68c3f7e2210726dbe05189004a64b8da435cc940
Bobby Singh <b830103@gmail.com>
2023-09-15 21:44:44 +0200

    3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css
    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ function newItem() {
 
   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
   function deleteListItem() {
-    li.classList.add("delete");
+    li.addClass.add("delete");
   }
   // 4. Reordering the items:
   $("#list").sortable();
